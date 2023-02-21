470a611baff57830b3ae66cc14fb502d488e7c62
PREETI2096 <preetirg96@gmail.com>
2023-02-21 10:09:57 +0530

    Added : multiple ride fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -2,17 +2,20 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
 	private static final double MIN_COST_PER_KILOMETER = 10;
-    private static final double COST_PER_TIME = 1;
-    private static final double MINIMUM_FARE = 5;
+	private static final double COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
 
-    public double calculateFare(double distance, double time) {
-        double cost = distance * MIN_COST_PER_KILOMETER + time * COST_PER_TIME;
-        if (cost < MINIMUM_FARE)
-            return MINIMUM_FARE ;
-        return cost;
-    }
-    public static void main(String[] args) {
-    	CabInvoiceGenerator cab = new CabInvoiceGenerator();
-    	System.out.println("Cost of fare: "+cab.calculateFare(25, 10));
+	public double calculateFare(double distance, double time) {
+		double cost = distance * MIN_COST_PER_KILOMETER + time * COST_PER_TIME;
+		if (cost < MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return cost;
 	}
+	 public double calculateFareForMultipleRides(Ride[] rides) {
+	        double cost = 0;
+	        for (Ride ride : rides) {
+	            cost = cost + this.calculateFare(ride.distance, ride.time);
+	        }
+	        return cost;
+	 }
 }

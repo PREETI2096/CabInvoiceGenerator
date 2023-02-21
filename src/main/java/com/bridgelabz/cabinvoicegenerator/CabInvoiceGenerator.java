ed3169d47da1f214367404a26fd6c3d03b3a0e03
PREETI2096 <preetirg96@gmail.com>
2023-02-21 10:24:09 +0530

    Added : enhanced fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -4,6 +4,7 @@ public class CabInvoiceGenerator {
 	private static final double MIN_COST_PER_KILOMETER = 10;
 	private static final double COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
+	  private static final double AVERAGE_FARE =2.0;
 
 	public double calculateFare(double distance, double time) {
 		double cost = distance * MIN_COST_PER_KILOMETER + time * COST_PER_TIME;
@@ -11,11 +12,20 @@ public class CabInvoiceGenerator {
 			return MINIMUM_FARE;
 		return cost;
 	}
-	 public double calculateFareForMultipleRides(Ride[] rides) {
-	        double cost = 0;
-	        for (Ride ride : rides) {
-	            cost = cost + this.calculateFare(ride.distance, ride.time);
-	        }
-	        return cost;
-	 }
+
+	public double calculateFareForMultipleRides(Ride[] rides) {
+		double cost = 0;
+		for (Ride ride : rides) {
+			cost = cost + this.calculateFare(ride.distance, ride.time);
+		}
+		return cost;
+	}
+
+	public InvoiceDescription calculateFareDescription(Ride[] rides) {
+		double cost = 0;
+		for (Ride ride : rides) {
+			cost = cost + this.calculateFare(ride.distance, ride.time);
+		}
+		return new InvoiceDescription(rides.length, cost, AVERAGE_FARE);
+	}
 }
--- src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorTest.java
@@ -5,33 +5,46 @@ import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
 public class CabInvoiceGeneratorTest {
-	 CabInvoiceGenerator  invoiceGenerator = new CabInvoiceGenerator();
-	    @BeforeAll
-	    public static void printBeforeAllMsg() {
-	        System.out.println("Welcome to the Cab Invoice Generator Program");
-	    }
-	    @AfterEach
-	    public void PrintAfterEachTestCasePass(){
-	        System.out.println("Test Passed");
-	    }
-	    @Test
-	    public void givenDistanceAndTime_shouldReturn_CalculateFare() {
-	        double distance = 2.0;
-	        double time = 5.0;
-	        double fare = invoiceGenerator.calculateFare(distance, time);
-	        Assert.assertEquals(25.0, fare, 0.0);
-	    }
-	    @Test
-	    public void givenLessDistanceAndTime_shouldReturn_MinimumCalculateFare() {
-	        double distance = 0.1;
-	        double time = 1;
-	        double fare = invoiceGenerator.calculateFare(distance, time);
-	        Assert.assertEquals(5, fare, 0.0);
-	    }
-	    @Test
-	    public void givenMultipleRides_shouldReturn_invoiceCost() {
-	        Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-	        double fare = invoiceGenerator.calculateFareForMultipleRides(rides);
-	        Assert.assertEquals(30.0, fare, 0.0);
-	    }
+	CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+
+	@BeforeAll
+	public static void printBeforeAllMsg() {
+		System.out.println("Welcome to the Cab Invoice Generator Program");
+	}
+
+	@AfterEach
+	public void PrintAfterEachTestCasePass() {
+		System.out.println("Test Passed");
+	}
+
+	@Test
+	public void givenDistanceAndTime_shouldReturn_CalculateFare() {
+		double distance = 2.0;
+		double time = 5.0;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25.0, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTime_shouldReturn_MinimumCalculateFare() {
+		double distance = 0.1;
+		double time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_shouldReturn_invoiceCost() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double fare = invoiceGenerator.calculateFareForMultipleRides(rides);
+		Assert.assertEquals(30.0, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_shouldReturn_invoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		InvoiceDescription actualDescription = invoiceGenerator.calculateFareDescription(rides);
+		InvoiceDescription expectedDescription = new InvoiceDescription(2, 30.0, 2.0);
+		Assert.assertEquals(expectedDescription, actualDescription);
+	}
 }
